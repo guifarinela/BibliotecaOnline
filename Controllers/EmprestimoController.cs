42f1a78040faa66e71cf3095649397d98c1bc8d2
guifarinela <guilhermefarinelass@gmail.com>
2024-04-12 17:34:12 -0300

    first commit

---
+++ Controllers/EmprestimoController.cs
@@ -0,0 +1,138 @@
+﻿using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Drawing.Charts;
+using EmprestimoLivros.Data;
+using EmprestimoLivros.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using DataTable = System.Data.DataTable;
+
+namespace EmprestimoLivros.Controllers
+{
+    public class EmprestimoController : Controller
+    {
+        readonly private ApplicationDbContext _db;
+
+        public EmprestimoController(ApplicationDbContext dbContext)
+        {
+            _db = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            IEnumerable<EmprestimosModel> emprestimos = _db.Emprestimos;
+            return View(emprestimos);
+        }
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(EmprestimosModel emprestimos) 
+        {
+            if (ModelState.IsValid) 
+            {
+                _db.Emprestimos.Add(emprestimos);
+                _db.SaveChanges();
+
+                TempData["MensagemSucesso"] = "Cadastro realizado com sucesso";
+
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int? id) 
+        {
+            if (id == null || id == 0) 
+            {
+                return NotFound();
+            }
+
+            EmprestimosModel emprestimos = _db.Emprestimos.FirstOrDefault(x => x.Id == id);
+
+            if (emprestimos == null) 
+            {
+                return NotFound();
+            }
+
+            return View(emprestimos);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(EmprestimosModel emprestimo)
+        {
+            if (ModelState.IsValid) 
+            {
+                _db.Emprestimos.Update(emprestimo);
+                _db.SaveChanges();
+                TempData["MensagemSucesso"] = "Edição realizada com sucesso";
+                return RedirectToAction("Index");
+            }
+            return View(emprestimo);
+        }
+
+        [HttpGet]
+        public IActionResult Excluir(int? id) 
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            EmprestimosModel emprestimo = _db.Emprestimos.FirstOrDefault(x => x.Id==id);
+
+            if (emprestimo == null) 
+            {
+                return NotFound();
+            }
+            _db.Emprestimos.Remove(emprestimo);
+            _db.SaveChanges();
+            TempData["MensagemSucesso"] = "Exclusão realizada com sucesso";
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult Exportar() 
+        {
+
+            var dados = GetDados();
+
+            using (XLWorkbook workbook = new XLWorkbook()) 
+            {
+                workbook.AddWorksheet(dados, "Dados Empréstimos");
+
+                using (MemoryStream ms = new MemoryStream()) 
+                {
+                    workbook.SaveAs(ms);
+                    return File(ms.ToArray(),"application/vnd.openxmlformats-officedocument.spredsheetml.sheet","Empréstimos.xml");
+                }
+            }
+        }
+
+        private DataTable GetDados()
+        {
+            DataTable datatable = new DataTable();
+
+            datatable.TableName = "Dados Empréstimos";
+            datatable.Columns.Add("Recebedor", typeof(string));
+            datatable.Columns.Add("Fornecedor", typeof(string));
+            datatable.Columns.Add("Livro", typeof(string));
+            datatable.Columns.Add("Data do Empréstimo", typeof(DateTime));
+
+            var dados = _db.Emprestimos.ToList();
+
+            if (dados.Count > 0) 
+            {
+                dados.ForEach(emprestimo =>
+                {
+                    datatable.Rows.Add(emprestimo.Recebedor, emprestimo.Fornecedor, emprestimo.LivroEmprestado,emprestimo.dataUltimaAtualizacao);
+                });
+            }
+
+            return datatable;
+        }
+    }
+}
